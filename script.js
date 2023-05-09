d4f0fbfed0893dddc1de148eff70df3ec68c6d89
ericDK89 <ericthr42@gmail.com>
2023-05-09 19:17:59 -0300

    add: branch 'exercicio_ajax'

--- script.js
+++ script.js
@@ -21,21 +21,4 @@ $(document).ready(function () {
       });
     },
   });
-
-  // fetch("https://api.github.com/users/ericDK89").then(async (res) => {
-  //   const data = await res.json();
-
-  //   // $(".profile-avatar").attr("src", data.avatar_url);
-  //   // $(".profile-name").text(data.name);
-  //   // $(".profile-username").text(`@${data.login}`);
-  //   // $("#followers-amount").text(data.followers);
-  //   // $("#following-amount").text(data.following);
-  //   // $("#github-profile-link").attr("href", data.html_url);
-
-  //   fetch(data.repos_url).then(async (res) => {
-  //     const data = await res.json();
-
-  //     $(".numbers-item").find("#repos-amount").text(data.length);
-  //   });
-  // });
 });
